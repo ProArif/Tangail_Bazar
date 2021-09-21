bbc5255a5357c81023bcad2211eef11b07f6b9e4
Md Ariful Islam <34057291+ProArif@users.noreply.github.com>
2021-09-21 15:13:09 +0600

    Initial commit

---
+++ app/src/main/java/com/nodeers/tangailbazar/MainActivity.java
@@ -0,0 +1,31 @@
+package com.nodeers.tangailbazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_getStarted;
+    private ImageView iv_learnMore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_getStarted = findViewById(R.id.btn_gs);
+        iv_learnMore = findViewById(R.id.learn_more);
+
+        btn_getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,EntryPoint.class));
+            }
+        });
+    }
+}
